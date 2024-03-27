8d7840c78a51ec83a462ae07a9458b72a1a60514
hellodit <asditap@gmail.com>
2024-03-27 22:35:11 +0700

    hide features list

--- packages/Webkul/Shop/src/Resources/views/components/layouts/features.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/features.blade.php
@@ -1,56 +1,36 @@
 {!! view_render_event('bagisto.shop.layout.features.before') !!}
-
 <!-- Features -->
-{{--<div class="container mt-20 max-lg:px-[30px] max-sm:mt-[30px]">--}}
-{{--    <div class="flex gap-[25px] justify-center max-lg:flex-wrap">--}}
-{{--        <div class="flex items-center gap-[20px]">--}}
-{{--            <span class="icon-truck flex items-center justify-center w-[60px] h-[60px] bg-white border border-black rounded-full text-[42px] text-navyBlue p-[10px]"></span>--}}
+<div class="container mt-20 max-lg:px-[30px] max-sm:mt-[30px]">
+    <div class="flex gap-[25px] justify-center max-lg:flex-wrap">
+        <div class="flex items-center gap-[20px]"> <span class="icon-truck flex items-center justify-center w-[60px] h-[60px] bg-white border border-black rounded-full text-[42px] text-navyBlue p-[10px]"></span>
 
-{{--            <div class="">--}}
-{{--                <p class="text-[16px] font-medium font-dmserif">Free Shipping</p>--}}
+            <div class="">
+                <p class="text-[16px] font-medium font-dmserif">Gratis Pendaftaran</p>
+                <p class="text-[14px] font-medium mt-[10px] text-[#6E6E6E] max-w-[217px]">Memudahkan user baru untuk daftar tanpa biaya</p>
+            </div>
+        </div>
+        <div class="flex items-center gap-[20px]"> <span class="icon-support flex items-center w-[60px] h-[60px] bg-white p-[10px] justify-center border border-black rounded-full text-[42px] text-navyBlue"></span>
 
-{{--                <p class="text-[14px] font-medium mt-[10px] text-[#6E6E6E] max-w-[217px]">--}}
-{{--                    If goods have problems, Lorem Ipsum is dummy text--}}
-{{--                </p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
+            <div class="">
+                <p class="text-[16px] font-medium font-dmserif">Gratis Posting/Upload Barang Jaminan</p>
+                <p class="text-[14px] font-medium mt-[10px] text-[#6E6E6E] max-w-[217px]">Gratis untuk anda yang memiliki barang jaminan yang akan digadaikan</p>
+            </div>
+        </div>
+        <div class="flex items-center gap-[20px]"> <span class="icon-dollar-sign flex items-center rounded-full w-[60px] h-[60px] p-[10px] justify-center border border-black bg-white text-[42px] text-navyBlue"></span>
 
-{{--        <div class="flex items-center gap-[20px]">--}}
-{{--            <span class="icon-support flex items-center w-[60px] h-[60px] bg-white p-[10px] justify-center border border-black rounded-full text-[42px] text-navyBlue"></span>--}}
+            <div class="">
+                <p class="text-[16px] font-medium font-dmserif">Product Disclaimer</p>
+                <p class="text-[14px] font-medium mt-[10px] text-[#6E6E6E] max-w-[217px]">Disclaimer mengenai hal-hal yang berkaitan dengan Posting/Upload Barang Jaminan</p>
+            </div>
+        </div>
+        <div class="flex items-center gap-[20px]"> <span class="icon-product flex items-center w-[60px] h-[60px] bg-white p-[10px] justify-center border border-black rounded-full text-[42px] text-navyBlue"></span>
 
-{{--            <div class="">--}}
-{{--                <p class="text-[16px] font-medium font-dmserif">Product Replace</p>--}}
-
-{{--                <p class="text-[14px] font-medium mt-[10px] text-[#6E6E6E] max-w-[217px]">--}}
-{{--                    If goods have problems, Lorem Ipsum is dummy text--}}
-{{--                </p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-{{--        <div class="flex items-center gap-[20px]">--}}
-{{--            <span class="icon-dollar-sign flex items-center rounded-full w-[60px] h-[60px] p-[10px] justify-center border border-black bg-white text-[42px] text-navyBlue"></span>--}}
-
-{{--            <div class="">--}}
-{{--                <p class="text-[16px] font-medium font-dmserif">Emi Available</p>--}}
-
-{{--                <p class="text-[14px] font-medium mt-[10px] text-[#6E6E6E] max-w-[217px]">--}}
-{{--                    If goods have problems, Lorem Ipsum is dummy text--}}
-{{--                </p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-{{--        <div class="flex items-center gap-[20px]">--}}
-{{--            <span class="icon-product flex items-center w-[60px] h-[60px] bg-white p-[10px] justify-center border border-black rounded-full text-[42px] text-navyBlue"></span>--}}
-
-{{--            <div class="">--}}
-{{--                <p class="text-[16px] font-medium font-dmserif">24/7 Support</p>--}}
-
-{{--                <p class="mt-[10px] text-[14px] text-[#6E6E6E] font-medium max-w-[217px]">--}}
-{{--                    If goods have problems, Lorem Ipsum is dummy text--}}
-{{--                </p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
+            <div class="">
+                <p class="text-[16px] font-medium font-dmserif">24/7 Support</p>
+                <p class="mt-[10px] text-[14px] text-[#6E6E6E] font-medium max-w-[217px]">jika ada kendala kami berikan bantuan melalui Grup Facebook Open Gadai</p>
+            </div>
+        </div>
+    </div>
+</div>
 
 {!! view_render_event('bagisto.shop.layout.features.after') !!}
